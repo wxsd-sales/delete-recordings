9002acc1d50fc7670954f459f3389e5af01a32c5
Rajitha Kantheti (rkanthet) <rkanthet@cisco.com>
2023-11-20 14:49:50 -0500

    Updates

---
+++ _app/immutable/chunks/paths.f91e8dc3.js
@@ -0,0 +1,1 @@
+var e;const t=((e=globalThis.__sveltekit_qoe8ze)==null?void 0:e.base)??"/delete-recordings";var s;const a=((s=globalThis.__sveltekit_qoe8ze)==null?void 0:s.assets)??t;export{a,t as b};
